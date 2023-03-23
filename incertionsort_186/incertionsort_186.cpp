05e4f17a93c0a7e00c6aff24a3e69f3d01a5f701
cahyadie <cahyadiilham12@gmail.com>
2023-03-23 15:46:43 +0700

    fix code 1

--- incertionsort_186/incertionsort_186.cpp
+++ incertionsort_186/incertionsort_186.cpp
@@ -51,7 +51,8 @@ void insertionsort()
         }
         arr[j + 1] + temp;
 
-        cout << "\nStep " << i << ":";
+        cout << endl;
+        cout << "\nPass " << i << ":";
         for (int k = 0; k < n; k++)
         {
             cout << arr[k] << " ";
